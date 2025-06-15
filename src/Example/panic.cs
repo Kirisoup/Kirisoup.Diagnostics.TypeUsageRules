9af864124d19fe613971d23d9eaa11cc67b7d5cc
Kirisoup <94152497+Kirisoup@users.noreply.github.com>
2025-06-15 20:24:18 +0800

    fix stupid typo

--- src/Example/panic.cs
+++ src/Example/panic.cs
@@ -5,19 +5,16 @@ _ = (Bar)new();
 _ = default(Bar);
 _ = (Bar)default;
 
-// _ = new Foo();
-// _ = (Foo)new();
-// _ = default(Foo);
-// _ = (Foo)default;
+_ = new Foo();
+_ = (Foo)new();
+_ = default(Foo);
+_ = (Foo)default;
 
 _ = new Foo(0);
 _ = (Foo)new(0);
 
 [NoDefault]
 [NoNew]
-readonly struct Foo
-{
-	public Foo(byte _) {}
-}
+readonly struct Foo(byte _);
 
 readonly struct Bar;
